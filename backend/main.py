51eeab209e233715c866d26b776b5c0a2aefeb01
Saloni Divase <salonidivase2007@gmail.com>
2026-04-07 22:47:39 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -209,3 +209,4 @@ if os.path.exists(frontend_build):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=7860, reload=False)
+p", host="0.0.0.0", port=7860, reload=False)
